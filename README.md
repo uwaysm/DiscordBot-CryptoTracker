d7e93d60f5688c040a72571bbaa9acd80f20408a
Uways Minty <66369956+uwaysm@users.noreply.github.com>
2023-03-22 21:28:30 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Crypto Price Tracker Bot
+
+This bot will automatically track the price of any cryptocurrency on coingecko through the discord bot's nickname. The nickname will update every 1 minute and reflect the price of the cryptocurrency being tracked.
+
+### What are the prerequisites for this guide?
+
+You must have an account for Discord [[Link](https://discordapp.com/developers/applications/)], GitHub [[Link](https://github.com/join)]
+
+### How do I create a bot and get a bot token?
+
+- Create an application in the developer portal by clicking [here](https://discordapp.com/developers/applications/)
+- Open up your new application and click 'Add Bot' under the Bot settings to create your bot.
+- After creating the bot, click the 'Copy' button under the title Token. Take note of your token as you will need it later.
+
+### How to choose the token to track?
+
+- Simply change the 'crypto_id' to the crypto id of the token you want to track.
+- Make sure to input your discord bot token on line 61: "YOUR_BOT_TOKEN"
---
+++ main.py
@@ -0,0 +1,62 @@
+import discord
+import requests
+import aiohttp
+import asyncio
+from pycoingecko import CoinGeckoAPI
+
+client = discord.Client(intents=discord.Intents.default())
+cg = CoinGeckoAPI()
+
+def fetch_crypto_price(crypto_id):
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        crypto_price = data[crypto_id]['usd']
+        return crypto_price
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+async def fetch_crypto_image_url(crypto_id):
+    url = f'https://api.coingecko.com/api/v3/coins/{crypto_id}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                image_url = data['image']['large']
+                return image_url
+            else:
+                print(f"Error: {response.status}")
+                return None
+
+async def update_bot_avatar(crypto_id):
+    image_url = await fetch_crypto_image_url(crypto_id)
+    if image_url:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as response:
+                if response.status == 200:
+                    image_data = await response.read()
+                    await client.user.edit(avatar=image_data)
+                else:
+                    print(f"Error: {response.status}")
+
+async def update_bot_nickname(crypto_id):
+    while True:
+        crypto_price = fetch_crypto_price(crypto_id)
+        if crypto_price:
+            nickname = f"{crypto_id.upper()}: ${crypto_price}"
+            for guild in client.guilds:
+                await guild.me.edit(nick=nickname)
+        await asyncio.sleep(1 * 60)  # Wait for 1 minute
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    crypto_id = 'cosmos'  # Change 'cosmos' to any other crypto ID to fetch its price and image
+    await update_bot_avatar(crypto_id)  # Update bot avatar only when the script is run
+    await update_bot_nickname(crypto_id)
+
+client.run('YOUR_BOT_TOKEN')
+
